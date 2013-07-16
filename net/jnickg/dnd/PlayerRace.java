3173f61898293a8f14958fc2610a3e38596de6ed
castlez <castlez@live.com>
2013-07-16 15:21:53 -0700

    *turned playerrace from enum to class with enum

--- net/jnickg/dnd/PlayerRace.java
+++ net/jnickg/dnd/PlayerRace.java
@@ -1,36 +1,37 @@
 package net.jnickg.dnd;
 
-/** This will one day store stuff for a player's race.
- * 
- */
-public enum PlayerRace {
-	HUMAN{
-		
-	},
+public class PlayerRace {
 	
-	DWARF{
+	public enum Race {
+		HUMAN{
+			
+		},
 		
-	},
-	
-	ELF{
+		DWARF{
+			
+		},
 		
-	},
-	
-	GNOME{
+		ELF{
+			
+		},
 		
-	},
-	
-	HALF_ELF{
+		GNOME{
+			
+		},
 		
-	},
-	
-	HALF_ORC{
+		HALF_ELF{
+			
+		},
 		
-	},
-	
-	HALFLING{
+		HALF_ORC{
+			
+		},
 		
-	};
+		HALFLING{
+			
+		};
 
-	//end of enum types--begin non member-specific stuff
+		//end of enum types--begin non member-specific stuff
+	}
+
 }
